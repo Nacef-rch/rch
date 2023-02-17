b210d52214b2e7aa36c92d992ffa4a770039a559
AzureAD\NacefRACHEH <nacef.racheh@satoripop.com>
2023-02-17 17:31:23 +0100

    feat(ui-web): :sparkles: adding layout

--- apps/rental-application-client-web/pages/index.tsx
+++ apps/rental-application-client-web/pages/index.tsx
@@ -1,22 +1,44 @@
 import styles from './index.module.scss';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 import { RootState } from '@rch/store';
 import Alert from '@mui/material/Alert';
-import { Button } from '@mui/material';
-import {pb,currentUser} from '@rch/pocketbase';
+import { Box, Button, Grid } from '@mui/material';
+import { pb, currentUser } from '@rch/pocketbase';
+import { SideBar, CardContainer } from '@rch/ui-web';
+
+
+import { Marker,MapContainer,TileLayer,Popup } from 'react-leaflet';
+
 
 export function Index() {
-  const test = useSelector((store: RootState)=>store.counter.value);
-  const login  = async () : Promise<void> => {
-    await pb.collection('users').authWithPassword('nacef','97084602');
-  }
+  const test = useSelector((store: RootState) => store.counter.value);
+  const login = async (): Promise<void> => {
+    await pb.collection('users').authWithPassword('nacef', '97084602');
+  };
   return (
-    <div className={styles.page}>
-        <Alert severity="error">{test}</Alert>
-        <h1 className="bg-indigo-500 p-2 font-mono text-white">Login with a click</h1>
-        <Button onClick={login}>Click me</Button>
-        
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      <SideBar />
+      <div id="MAP">
+        <Grid container spacing={2} sx={{ flexGrow: 1, p: 3 }}>
+          <Grid item xs={6} md={6}>
+            <CardContainer />
+          </Grid>
+          <Grid item xs={6} md={6}>
+            {/* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer> */}
+          </Grid>
+        </Grid>
+      </div>
+    </Box>
   );
 }
 
---
+++ libs/ui-web/src/layout/CardContainer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export function CardContainer() {
+  return (
+    <div className="shadow bg-white rounded-lg">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit maiores, quam praesentium
+      ratione debitis laboriosam architecto maxime asperiores repellendus vitae, aliquam expedita
+      qui quo. Dignissimos delectus natus tenetur repellat nihil doloremque molestias.
+      Reprehenderit, nostrum modi totam molestias quo nemo eveniet nesciunt, soluta aut cum
+      voluptatum aspernatur veritatis pariatur provident amet.
+    </div>
+  );
+}
+
+export default CardContainer;
